b6d8f02fd1fd44eef3adcf06b34aaeb7fe639d9b
gaeng2y <gaeng2y@gmail.com>
2023-06-10 14:10:35 +0900

    💄 호선별 색 SubwayLine에 추가

--- Subt/Subt/Models/SubwayLine.swift
+++ Subt/Subt/Models/SubwayLine.swift
@@ -25,4 +25,47 @@ enum SubwayLine: String, Decodable {
     case 수인분당선 = "1075"
     case 신분당선 = "1077"
     case 우이신설선 = "1092"
+    
+    var subwayColor: Color {
+        let color: Color =
+        switch self {
+        case .일호선:
+            Color("Line1")
+        case .이호선:
+            Color("Line2")
+        case .삼호선:
+            Color("Line3")
+        case .사호선:
+            Color("Line4")
+        case .오호선:
+            Color("Line5")
+        case .육호선:
+            Color("Line6")
+        case .칠호선:
+            Color("Line7")
+        case .팔호선:
+            Color("Line8")
+        case .구호선:
+            Color("Line9")
+        case .중앙선:
+            Color("Line1")
+        case .경의중앙선:
+            Color("LineKyeongYi")
+        case .공항철도:
+            Color("LineAirport")
+        case .경춘선:
+            Color("LineKyeongChun")
+        case .수인분당선:
+            Color("LineBundang")
+        case .신분당선:
+            Color("LineSinBundang")
+        case .우이신설선:
+            Color.black
+        }
+        return color
+    }
+}
+
+extension Color {
+    
 }
